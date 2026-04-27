3184f1ef49b44bd9f6b1730ab1abcf5d3cb267b4
Rich Hugo Hutagalung <149813351+Gotari@users.noreply.github.com>
2026-04-28 02:05:10 +0700

    tambah route dan hilangkan bbrp tombol

--- lib/profil.dart
+++ lib/profil.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:m05/about.dart';
+import 'package:m05/editProfil.dart';
+import 'package:m05/notifikasi.dart';
+import 'package:m05/riwayatPembayaran.dart';
 
 class Profil extends StatefulWidget {
   const Profil({Key? key}) : super(key: key);
@@ -8,13 +12,11 @@ class Profil extends StatefulWidget {
 }
 
 class _ProfilState extends State<Profil> {
+  String namaUser = "Bryan Lauda";
   final List<Map<String, dynamic>> menuItems = [
     {'icon': Icons.edit, 'title': 'Edit Profil'},
-    {'icon': Icons.lock, 'title': 'Ganti Password'},
-    {'icon': Icons.school, 'title': 'Riwayat Akademik'},
     {'icon': Icons.receipt, 'title': 'Riwayat Pembayaran'},
     {'icon': Icons.notifications, 'title': 'Notifikasi'},
-    {'icon': Icons.settings, 'title': 'Pengaturan'},
     {'icon': Icons.info, 'title': 'About'},
   ];
 
@@ -27,27 +29,25 @@ class _ProfilState extends State<Profil> {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color.fromARGB(255,212,187,255),
-              Color.fromARGB(255,187,212,255),
-              Color.fromARGB(255,229,219,255),
+              Color.fromARGB(255, 212, 187, 255),
+              Color.fromARGB(255, 187, 212, 255),
+              Color.fromARGB(255, 229, 219, 255),
             ],
           ),
         ),
         child: SingleChildScrollView(
           child: Column(
             children: [
-
-              SizedBox(height:20),
+              SizedBox(height: 20),
 
               // Header
               Padding(
-                padding: EdgeInsets.symmetric(horizontal:15),
+                padding: EdgeInsets.symmetric(horizontal: 15),
                 child: Row(
-                  mainAxisAlignment:
-                      MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     GestureDetector(
-                      onTap: (){
+                      onTap: () {
                         Navigator.pop(context);
                       },
                       child: Icon(
@@ -61,46 +61,38 @@ class _ProfilState extends State<Profil> {
                       padding: EdgeInsets.all(8),
                       decoration: BoxDecoration(
                         color: Colors.red,
-                        borderRadius:
-                           BorderRadius.circular(50),
+                        borderRadius: BorderRadius.circular(50),
                       ),
-                      child: Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                      ),
+                      child: Icon(Icons.notifications, color: Colors.white),
                     ),
                   ],
                 ),
               ),
 
-              SizedBox(height:35),
+              SizedBox(height: 35),
 
               // CONTAINER BESAR UNGU
               Transform.translate(
-                offset: Offset(0,-20),
+                offset: Offset(0, -20),
                 child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal:20,
-                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
 
                   child: Container(
                     padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
                       color: Colors.deepPurple,
-                      borderRadius:
-                          BorderRadius.circular(30),
+                      borderRadius: BorderRadius.circular(30),
                       boxShadow: [
                         BoxShadow(
                           blurRadius: 12,
                           color: Colors.black26,
-                          offset: Offset(0,6),
-                        )
+                          offset: Offset(0, 6),
+                        ),
                       ],
                     ),
 
                     child: Column(
                       children: [
-
                         // Card user
                         Container(
                           width: double.infinity,
@@ -110,124 +102,145 @@ class _ProfilState extends State<Profil> {
                           ),
                           decoration: BoxDecoration(
                             color: Colors.white,
-                            borderRadius:
-                               BorderRadius.circular(20),
+                            borderRadius: BorderRadius.circular(20),
                           ),
 
                           child: Column(
                             children: [
-
                               CircleAvatar(
-                                radius:50,
-                                backgroundImage:
-                                   AssetImage(
+                                radius: 50,
+                                backgroundImage: AssetImage(
                                   'assets/user_avatar.jpg',
                                 ),
                               ),
 
-                              SizedBox(height:15),
+                              SizedBox(height: 15),
 
                               Container(
-                                padding:
-                                  EdgeInsets.symmetric(
-                                    horizontal:12,
-                                    vertical:6,
-                                  ),
-                                decoration:
-                                  BoxDecoration(
-                                    color: Colors.grey[200],
-                                    borderRadius:
-                                      BorderRadius.circular(20),
-                                  ),
-                                child: Text(
-                                  "CARD INFO USER",
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 12,
+                                  vertical: 6,
                                 ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text("CARD INFO USER"),
                               ),
 
-                              SizedBox(height:15),
+                              SizedBox(height: 15),
 
                               Text(
-                                "Bryan Lauda",
+                                namaUser,
                                 style: TextStyle(
-                                  fontSize:22,
-                                  fontWeight:
-                                    FontWeight.bold,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
 
-                              SizedBox(height:5),
+                              SizedBox(height: 5),
 
                               Text("24113088"),
 
-                              SizedBox(height:5),
+                              SizedBox(height: 5),
 
-                              Text(
-                                "Jurusan Teknik Informasi",
-                              ),
+                              Text("Jurusan Teknik Informasi"),
 
-                              SizedBox(height: 5,)
+                              SizedBox(height: 5),
                             ],
                           ),
                         ),
 
-                        SizedBox(height:20),
+                        SizedBox(height: 20),
 
                         // Menu
-                        ...menuItems.map((item){
+                        ...menuItems.map((item) {
                           return Padding(
-                            padding:
-                              EdgeInsets.only(bottom:12),
-                            child: Container(
-                              padding:
-                                EdgeInsets.all(15),
+                            padding: EdgeInsets.only(bottom: 12),
+                            child: GestureDetector(
+                              onTap: () async {
+                                if (item['title'] == 'Edit Profil') {
+                                  final namaBaru = await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          EditProfilPage(namaLama: namaUser),
+                                    ),
+                                  );
 
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                  BorderRadius.circular(15),
-                              ),
+                                  if (namaBaru != null &&
+                                      namaBaru.toString().isNotEmpty) {
+                                    setState(() {
+                                      namaUser = namaBaru;
+                                    });
+                                  }
+                                } else if (item['title'] ==
+                                    'Riwayat Pembayaran') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => RiwayatPembayaran(),
+                                    ),
+                                  );
+                                } else if (item['title'] == 'Notifikasi') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => NotifikasiPage(),
+                                    ),
+                                  );
+                                } else if (item['title'] == 'About') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => AboutPage(),
+                                    ),
+                                  );
+                                }
+                              },
 
-                              child: Row(
-                                children: [
+                              child: Container(
+                                padding: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
 
-                                  Container(
-                                    width:40,
-                                    height:40,
-                                    decoration:
-                                      BoxDecoration(
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 40,
+                                      height: 40,
+                                      decoration: BoxDecoration(
                                         color: Colors.purple[100],
-                                        borderRadius:
-                                          BorderRadius.circular(10),
+                                        borderRadius: BorderRadius.circular(10),
                                       ),
-                                    child: Icon(
-                                      item['icon'],
-                                      color: Colors.deepPurple,
-                                    ),
-                                  ),
-
-                                  SizedBox(width:15),
-
-                                  Expanded(
-                                    child: Text(
-                                      item['title'],
-                                      style: TextStyle(
-                                        fontWeight:
-                                          FontWeight.w600,
+                                      child: Icon(
+                                        item['icon'],
+                                        color: Colors.deepPurple,
                                       ),
                                     ),
-                                  ),
 
-                                  Icon(
-                                    Icons.arrow_forward_ios,
-                                    size:16,
-                                  ),
-                                ],
+                                    SizedBox(width: 15),
+
+                                    Expanded(
+                                      child: Text(
+                                        item['title'],
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+
+                                    Icon(Icons.arrow_forward_ios, size: 16),
+                                  ],
+                                ),
                               ),
                             ),
                           );
                         }).toList(),
 
-                        SizedBox(height:20),
+                        SizedBox(height: 20),
 
                         // Logout
                         SizedBox(
@@ -235,40 +248,30 @@ class _ProfilState extends State<Profil> {
                           child: ElevatedButton(
                             onPressed: () {},
 
-                            style:
-                              ElevatedButton.styleFrom(
-                                backgroundColor:
-                                   Colors.white,
-                                padding:
-                                  EdgeInsets.symmetric(
-                                    vertical:15,
-                                  ),
-                                shape:
-                                  RoundedRectangleBorder(
-                                    borderRadius:
-                                      BorderRadius.circular(15),
-                                  ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
                               ),
+                            ),
 
                             child: Text(
                               "Log Out",
                               style: TextStyle(
                                 color: Colors.deepPurple,
-                                fontWeight:
-                                   FontWeight.bold,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                        )
-
+                        ),
                       ],
                     ),
                   ),
                 ),
               ),
 
-              SizedBox(height:30),
-
+              SizedBox(height: 30),
             ],
           ),
         ),
